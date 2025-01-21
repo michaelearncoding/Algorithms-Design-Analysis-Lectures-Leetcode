73690d109ab569b199e9cdb49d444638ccac1066
Michael(Qingda) Mai <michael.mai@uwaterloo.ca>
2025-01-21 18:43:01 -0500

    linked list skill2 - dived & conquer

--- Cpp-version/linkedList.cpp
+++ Cpp-version/linkedList.cpp
@@ -1,3 +1,48 @@
-//
-// Created by Qingda Mai on 2025-01-21.
-//
+#include <iostream>
+
+// Your existing code
+struct ListNode {
+    int val;
+    ListNode* next;
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+ListNode* findFromEnd(ListNode* head, int k) {
+    ListNode* p1 = head;
+    for (int i = 0; i < k; i++) {
+        p1 = p1->next;
+    }
+    ListNode* p2 = head;
+    while (p1 != nullptr) {
+        p2 = p2->next;
+        p1 = p1->next;
+    }
+    return p2;
+}
+
+// Main function to test the findFromEnd function
+int main() {
+    // Create a linked list: 1 -> 2 -> 3 -> 4 -> 5
+    ListNode* head = new ListNode(1);
+    head->next = new ListNode(2);
+    head->next->next = new ListNode(3);
+    head->next->next->next = new ListNode(4);
+    head->next->next->next->next = new ListNode(5);
+
+    // Find the 2nd node from the end
+    ListNode* result = findFromEnd(head, 2);
+    if (result != nullptr) {
+        std::cout << "The value of the 2nd node from the end is: " << result->val << std::endl;
+    } else {
+        std::cout << "The list is shorter than 2 nodes." << std::endl;
+    }
+
+    // Clean up the allocated memory
+    while (head != nullptr) {
+        ListNode* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    return 0;
+}
--- LinkedListSkills/7wonders.py
+++ LinkedListSkills/7wonders.py
@@ -180,3 +180,22 @@ current = merged_list
 while current:
     print(current.val, end=" -> ")
     current = current.next
+
+
+
+
+# 单链表的倒数第k个节点
+
+def findFromEnd(head: ListNode, k: int) -> ListNode:
+    p1 = head
+    # p1 先走 k 步
+    for i in range(k):
+        p1 = p1.next
+    p2 = head
+    # p1 和 p2 同时走 n - k 步
+    while p1 != None:
+        p2 = p2.next
+        p1 = p1.next
+    # 若p1 走到了尾节点，p2 正好走到了倒数第 k 个节点
+    # p2 现在指向第 n - k + 1 个节点，即倒数第 k 个节点
+    return p2
