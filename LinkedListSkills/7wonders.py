965024085f682253a3ad9f31a10d00632bcf230f
Michael(Qingda) Mai <michael.mai@uwaterloo.ca>
2025-01-21 15:52:57 -0500

    linked list skill2 - dived & conquer

--- LinkedListSkills/7wonders.py
+++ LinkedListSkills/7wonders.py
@@ -1,3 +1,4 @@
+# 合并两个有序链表
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
         # 虚拟头结点
@@ -27,7 +28,36 @@ class Solution:
         return dummy.next
     
 # l1, l2 类似于拉链两侧的锯齿，指针 p 就好像拉链的拉索，将两个有序链表合并。
-# 
+
+# 单链表的分解
+class Solution:
+    def partition(self, head: ListNode, x: int) -> ListNode:
+        # 存放小于 x 的链表的虚拟头结点
+        dummy1 = ListNode(-1)
+        # 存放大于等于 x 的链表的虚拟头结点
+        dummy2 = ListNode(-1)
+        # p1, p2 指针负责生成结果链表
+        p1, p2 = dummy1, dummy2
+        # p 负责遍历原链表，类似合并两个有序链表的逻辑
+        # 这里是将一个链表分解成两个链表
+        p = head
+        while p:
+            if p.val >= x:
+                p2.next = p
+                p2 = p2.next
+            else:
+                p1.next = p
+                p1 = p1.next
+            # 不能直接让 p 指针前进，
+            # p = p.next
+            # 断开原链表中的每个节点的 next 指针
+            temp = p.next
+            p.next = None
+            p = temp
+        # 连接两个链表
+        p1.next = dummy2.next
+
+        return dummy1.next
 
 
 
