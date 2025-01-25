cc9ae0b0a9a9712ec0d47ce0d83b550d4d7b4d05
Michael(Qingda) Mai <michael.mai@uwaterloo.ca>
2025-01-24 21:32:35 -0500

    adding the specific process of solving the bfs graph

--- bfs_Graph.py
+++ bfs_Graph.py
@@ -19,3 +19,77 @@ def bfs(graph, s):
         step += 1  # 每遍历完一层，步数加一
     
     return -1  # 如果遍历完所有节点仍未找到目标节点，返回 -1
+
+# 记录一维字符串的相邻索引
+neighbor = [
+    [1, 3],
+    [0, 4, 2],
+    [1, 5],
+    [0, 4],
+    [3, 1, 5],
+    [4, 2]
+]
+
+
+# 滑动谜题：
+
+from collections import deque
+
+class Solution:
+    def slidingPuzzle(self, board):
+        target = "123450"
+        # 将 2x3 的数组转化成字符串作为 BFS 的起点
+        start = ""
+        for i in range(len(board)):
+            for j in range(len(board[0])):
+                start += str(board[i][j])
+        
+        # ****** BFS 算法框架开始 ******
+        q = deque()
+        visited = set()
+        # 从起点开始 BFS 搜索
+        q.append(start)
+        visited.add(start)
+        
+        step = 0
+        while q: 
+            # 当前层的节点数量
+            sz = len(q)
+            for _ in range(sz):
+                cur = q.popleft()
+                # 判断是否达到目标局面
+                if cur == target:
+                    return step
+                # 将数字 0 和相邻的数字交换位置
+                for neighbor_board in self.getNeighbors(cur):
+                    # 防止走回头路
+                    if neighbor_board not in visited:
+                        q.append(neighbor_board)
+                        visited.add(neighbor_board)
+            step += 1
+        # ****** BFS 算法框架结束 ******
+        return -1
+
+    def getNeighbors(self, board):
+        # 记录一维字符串的相邻索引
+        mapping = [
+            [1, 3],
+            [0, 4, 2],
+            [1, 5],
+            [0, 4],
+            [3, 1, 5],
+            [4, 2]
+        ] 
+        
+        idx = board.index('0')
+        neighbors = []
+        for adj in mapping[idx]:
+            new_board = self.swap(board, idx, adj)
+            neighbors.append(new_board)
+        return neighbors
+
+    def swap(self, board, i, j):
+        chars = list(board)
+        chars[i], chars[j] = chars[j], chars[i]
+        return ''.join(chars)
+
